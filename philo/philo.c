baf3fe78cc284487f71dab5b33f9bd4c5d773009
yoamzil <amzilyouness@gmail.com>
2023-06-09 00:20:22 +0100

    Starting...

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,23 @@
 /*   By: yoamzil <yoamzil@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 01:36:27 by yoamzil           #+#    #+#             */
-/*   Updated: 2023/06/08 01:36:28 by yoamzil          ###   ########.fr       */
+/*   Updated: 2023/06/09 00:19:55 by yoamzil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+
+void	*philo(void *arg)
+{
+    t_philo	*philo;
+
+    philo = (t_philo*)arg;
+    while (1)
+    {
+        // philo_eat(philo);
+        // philo_sleep(philo);
+        // philo_think(philo);
+    }
+    return (NULL);
+}
