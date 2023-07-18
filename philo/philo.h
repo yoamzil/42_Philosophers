7ce8a01f6d96f45e63c6962f35925709332d033b
yoamzil <amzilyouness@gmail.com>
2023-07-18 18:29:03 +0100

    Added another struct

--- philo/philo.h
+++ philo/philo.h
@@ -2,11 +2,22 @@
 #define PHILO_H
 
 #include <pthread.h>
+#include <stdio.h>
 
 typedef struct s_philo
 {
     int num;
     int id;
+    struct s_data *data;
 } t_philo;
+
+typedef struct s_data
+{
+    int num_of_philos;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int must_eat;
+} t_data;
 
 #endif
